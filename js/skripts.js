2f328c53295070e40f7d73c11bd7fd96431b6789
nikita-malevanec <nikitamalevanec@yahoo.com>
2024-05-19 22:48:52 +0200

    Homework #14

--- js/skripts.js
+++ js/skripts.js
@@ -16,3 +16,31 @@ $('.owl-carousel').owlCarousel({
 $( function() {
     $( "#accordion" ).accordion();
 } );
+
+// $('.owl-carousel-1').owlCarousel({
+//     autoplay:true,
+//     autoplayTimeout:1000,
+//     loop: true,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         500:{
+//             items:2
+//         },
+//         700:{
+//             items:2
+//         },
+//         1100:{
+//             items:4
+//         }
+//     }
+// });
+// $('.owl-carousel-2').owlCarousel({
+//     autoplay:true,
+//     autoplayTimeout:1000,
+//     loop: true,
+//     nav:true,
+//
+// });
